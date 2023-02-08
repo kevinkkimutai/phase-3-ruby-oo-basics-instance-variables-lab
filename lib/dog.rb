3c555983aef32629a4d5ddb71d34a981bc809a76
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-08 10:40:30 +0300

    Assignment completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-
+    # setter method
+def name= (dog_name)
+    # assign  the @name instance variable a value
+    @this_dogs_name = dog_name
 end
+# gutter method
+def name
+    # access data from the @name instance variable and retun it
+    @this_dogs_name
+end
+end
+lassie = Dog.new
+# call the dog name= method with a value of Lassie
+lassie.name = "Lassie"
+# this is equivalent to calling lassie.name=("Lassie")
+puts lassie.name
